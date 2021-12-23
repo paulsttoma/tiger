80b453597339874400fc1a13ca742e48442a0fcb
Pauls Toma <94072281+paulsttoma@users.noreply.github.com>
2021-12-23 15:37:20 -0500

    javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 
-
 function App() {
   return (
     <div className="App-header">
       <p>Hello World Pauls Toma</p>
+      <button onClick = "safeMint.js"> mint </button>
     </div>
   );
 }
