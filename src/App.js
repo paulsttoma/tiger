f0ebb35ba87bca21ef785551f70922d93164bacc
Dev <pauls@Paulss-MacBook-Pro.local>
2021-12-23 13:37:18 -0500

    new 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src= "image.png" alt="logo"/>
-        <p>
-          Tiger for Attractions <br /> Currently learning... 12 more hours 
-        </p>
-      </header>
+    <div className="Toma">
+      <p>Hi</p>
     </div>
   );
 }
